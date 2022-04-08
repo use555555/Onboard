4226358c1c44a2a539b870198bd8f0def22a8182
Pawarich Ngamvuttivorn <42509276+use555555@users.noreply.github.com>
2022-04-08 19:44:59 +0700

    Scroll index test

--- Basic/FLTK/Helloworld.py
+++ Basic/FLTK/Helloworld.py
@@ -3,10 +3,9 @@ import sys
 import time
 
 def callBack(widget):
-    if widget == SButton:
-        if scroll.children() > 1:
-            scroll.clear()
-            window.redraw()
+    print(scroll.find(widget))
+    scroll.remove(widget)
+    window.redraw()
     pass
 
 
@@ -28,29 +27,33 @@ Button = Fl_Button(  300, 40, 260, 100, "Time to dance"  )
 Button.labelsize( 24 )
 Button.labelfont( FL_BOLD+FL_ITALIC )
 Button.labeltype( FL_SHADOW_LABEL )
+Button.callback(callBack)
 
 Input = Fl_Input( 20, 160, 260, 100 )
 Input.textsize( 30 )
 Output = Fl_Output( 300, 160, 260, 100 )
 Output.textsize( 30 )
 
-SButton = Fl_Button(  580, 0, 200, 100, "Test"  )
-SButton.labelsize( 24 )
-SButton.labelfont( FL_BOLD+FL_ITALIC )
-SButton.labeltype( FL_SHADOW_LABEL )
-SButton.callback(callBack)
-
 scroll = Fl_Scroll( 580, 0, 200, 556 )
-
-scroll.add(SButton)
+scroll.end()
+buttonPos = 0
+for i in range(0, 2):
+    SButton = Fl_Button(580, buttonPos, 200, 100, "Test")
+    SButton.labelsize(24)
+    SButton.labelfont(FL_BOLD + FL_ITALIC)
+    SButton.labeltype(FL_SHADOW_LABEL)
+    SButton.callback(callBack)
+    scroll.add(SButton)
+    buttonPos += 100
 
 TimeStamp = time.time()
+
 
 # Show the window and enter the FLTK event loop
 # window.end()
 window.show(sys.argv)
 while Fl.check() > 0:
-    print(scroll.children())
+    # print(scroll.children())
     if Button.value():
         if Input.value() == "Mom is here":
             if time.time() - TimeStamp >= 1/30:
